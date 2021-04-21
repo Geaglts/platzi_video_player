81951c20b21391def033bb2ad8f75b68f9aabacb
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-04-20 20:45:12 -0500

    Ya funciona el mute y unmute

--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,8 @@ import AutoPlay from "./plugins/AutoPlay.js";
 const video = document.querySelector("video");
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector("button");
-button.onclick = () => player.togglePlay();
+const togglePlayBtn = document.querySelector("#togglePlay");
+const toggleMuteBtn = document.querySelector("#toggleMute");
+
+togglePlayBtn.onclick = () => player.togglePlay();
+toggleMuteBtn.onclick = () => player.toggleMute();
